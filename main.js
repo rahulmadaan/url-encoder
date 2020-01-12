3acd9c6564bb6862f9f80b1befc8bef266235f50
Rahul Madaan <rahulma@thoughtworks.com>
2020-01-12 22:42:09 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const url_json_encoder = potential_url => {
+  return potential_url
+    .replace(/ /g, "%20")
+    .replace(/"/g, "%22")
+    .replace(/'/g, "%E2%80%99")
+    .replace(/{/g, "%7B")
+    .replace(/}/g, "%7D")
+    .replace(/\[/g, "%5B")
+    .replace(/\]/g, "%5D")
+    .replace(/:/g, "%3A")
+    .replace(/,/g, "%2C")
+    .replace(/\\/g, "%5C")
+    .replace(/\\n/g, "");
+};
+
